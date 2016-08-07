32dbf406858189ceb9d19a552ab991e6d9b8c695
NaoeS <hitsolb111081@gmail.com>
2016-08-08 00:34:34 +0900

    フロントのエンドポイントを復活

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from app import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     # スポット
     url(r'^v1/spots/$', views.spot_list, name='spot_list'),     # 一覧
 ]
