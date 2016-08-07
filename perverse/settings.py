f2eb5de93e18cd8479aca59fc5cce3ddc987378e
shimizu <l.bird0206@gmail.com>
2016-08-07 22:38:48 +0900

    mysqlに変更

--- perverse/settings.py
+++ perverse/settings.py
@@ -76,13 +76,22 @@ WSGI_APPLICATION = 'perverse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+#  DATABASES = {
+    #  'default': {
+        #  'ENGINE': 'django.db.backends.sqlite3',
+        #  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #  }
+#  }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'perverse',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '8889',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
