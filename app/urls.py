6d1375944a45f0214d33d7562c3c58286709091f
NaoeS <hitsolb111081@gmail.com>
2016-08-08 00:28:07 +0900

    スポット一覧取得作成

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-
-from . import views
+from app import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # スポット
+    url(r'^v1/spots/$', views.spot_list, name='spot_list'),     # 一覧
 ]
-
--- app/views.py
+++ app/views.py
@@ -1,5 +1,36 @@
+import json
+from collections import OrderedDict
 from django.http import HttpResponse
-from django.shortcuts import render
+from app.models import Spot
 
-def index(request):
-    return render(request, 'index.html')
+
+def render_json_response(request, data, status=None):
+    """response を JSON で返却"""
+    json_str = json.dumps(data, ensure_ascii=False, indent=2)
+    callback = request.GET.get('callback')
+    if not callback:
+        callback = request.POST.get('callback')  # POSTでJSONPの場合
+    if callback:
+        json_str = "%s(%s)" % (callback, json_str)
+        response = HttpResponse(json_str, content_type='application/javascript; charset=UTF-8', status=status)
+    else:
+        response = HttpResponse(json_str, content_type='application/json; charset=UTF-8', status=status)
+    return response
+
+
+def spot_list(request):
+    """スポットのJSONを返す"""
+    spots = []
+    for spot in Spot.objects.all().order_by('id'):
+        spot_dict = OrderedDict([
+            ('id', spot.id),
+            ('name', spot.name),
+            ('lat', spot.lat),
+            ('long', spot.long),
+            ('description', spot.description),
+            ('reach_count', spot.reach_count)
+        ])
+        spots.append(spot_dict)
+
+    data = OrderedDict([ ('spots', spots) ])
+    return render_json_response(request, data)
