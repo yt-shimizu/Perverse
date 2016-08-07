bfad8c2e38e309bfadbfd0c0c85311d355695eef
NaoeS <hitsolb111081@gmail.com>
2016-08-08 06:13:26 +0900

    Count モデル作成

--- app/apis.py
+++ app/apis.py
@@ -1,10 +1,15 @@
 from rest_framework import viewsets, routers
-from app.models import Spot
-from app.serializers import SpotSerializer
+from app.models import Spot, Count
+from app.serializers import SpotSerializer, CountSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Spot.objects.all()
     serializer_class = SpotSerializer
 
+class CountViewSet(viewsets.ModelViewSet):
+    queryset = Count.objects.all()
+    serializer_class = CountSerializer
+
 router = routers.DefaultRouter()
 router.register(r'spots', PostViewSet)
+router.register(r'counts', CountViewSet)
--- app/serializers.py
+++ app/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
-from app.models import Spot
+from app.models import Spot, Count
 
 class SpotSerializer(serializers.ModelSerializer):
+    count = serializers.SerializerMethodField()
     class Meta:
         model = Spot
+
+    def get_count(self, obj):
+        return obj.spot.count()
+
+class CountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Count
